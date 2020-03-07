066b915d217867acfa70ea3bf91d9b177cb411cb
defolt17 <defolt_17@mail.ru>
2020-03-07 16:26:47 +0300

    Unfortinatelly had to remove v-model in mini-variant ;<

--- web_ui/src/App.vue
+++ web_ui/src/App.vue
@@ -1,6 +1,13 @@
 <template>
 	<v-app id="web_ui">
-		<v-app-bar app clipped-left dense flat hide-on-scrol overlap>
+		<v-app-bar
+			app
+			clipped-left
+			dense
+			flat
+			hide-on-scrol
+			overlap
+		>
 			<v-img
 				src="projectIconWhite.png"
 				max-height="40"
@@ -10,7 +17,12 @@
 			<v-toolbar-title>NROS</v-toolbar-title>
 			<v-spacer></v-spacer>
 
-			<v-menu absolute offset-y close-on-click transition="scale-transition">
+			<v-menu
+				absolute
+				offset-y
+				close-on-click
+				transition="scale-transition"
+			>
 				<template
 					v-slot:activator="{
 						on
@@ -24,7 +36,11 @@
 						dark
 						v-on="on"
 					>
-						<v-badge overlap color="green" :content="notificationBadgeNum">
+						<v-badge
+							overlap
+							color="green"
+							:content="notificationBadgeNum"
+						>
 							<v-icon>mdi-bell </v-icon>
 						</v-badge>
 					</v-btn>
@@ -34,12 +50,15 @@
 					<v-list-item
 						link
 						dense
-						v-for="(userMenuItem, index) in notificationItems"
+						v-for="(userMenuItem,
+						index) in notificationItems"
 						:key="index"
 						router
 						:to="userMenuItem.link"
 					>
-						<v-list-item-title>{{ userMenuItem.title }}</v-list-item-title>
+						<v-list-item-title>{{
+							userMenuItem.title
+						}}</v-list-item-title>
 					</v-list-item>
 				</v-list>
 			</v-menu>
@@ -53,12 +72,16 @@
 					<v-btn
 						text
 						color="red"
-						@click.stop="userMenuExpanded = !userMenuExpanded"
+						@click.stop="
+							userMenuExpanded = !userMenuExpanded
+						"
 						dark
 						v-on="on"
 					>
 						User name
-						<v-icon right>{{ changeArrowOnExpandUserMenu }}</v-icon>
+						<v-icon right>{{
+							changeArrowOnExpandUserMenu
+						}}</v-icon>
 					</v-btn>
 				</template>
 
@@ -66,7 +89,8 @@
 					<v-list-item
 						link
 						dense
-						v-for="(notificationItem, index) in userMenuItems"
+						v-for="(notificationItem,
+						index) in userMenuItems"
 						:key="index"
 						router
 						:to="notificationItem.link"
@@ -76,7 +100,8 @@
 						}}</v-list-item-title>
 					</v-list-item>
 					<v-list-item link dense router>
-						<v-list-item-title @click="logOutSnackbar = true"
+						<v-list-item-title
+							@click="logOutSnackbar = true"
 							>Log out</v-list-item-title
 						>
 					</v-list-item>
@@ -85,29 +110,25 @@
 		</v-app-bar>
 
 		<v-navigation-drawer
+			v-model="sidebar"
+			hide-overlay
 			mini-variant-width="50px"
 			app
 			clipped
 			permanent
-			v-bind:mini-variant="drawerExpanded"
+			expand-on-hover
 			floating
 			width="170px"
 		>
 			<v-list dense>
-				<v-list-item link @click.stop="drawerExpanded = !drawerExpanded" dense>
-					<v-list-item-action>
-						<v-icon>{{ changeArrowOnExpandDrawer }}</v-icon>
-					</v-list-item-action>
-					<v-list-item-content>
-						<v-list-item-title>Close</v-list-item-title>
-					</v-list-item-content>
-				</v-list-item>
 				<v-list-item link dense to="/main">
 					<v-list-item-action>
 						<v-icon>mdi-home-variant</v-icon>
 					</v-list-item-action>
 					<v-list-item-content>
-						<v-list-item-title>Main menu</v-list-item-title>
+						<v-list-item-title
+							>Main menu</v-list-item-title
+						>
 					</v-list-item-content>
 				</v-list-item>
 				<v-list-item link dense to="/charts">
@@ -115,23 +136,33 @@
 						<v-icon>mdi-chart-arc</v-icon>
 					</v-list-item-action>
 					<v-list-item-content>
-						<v-list-item-title>Charts</v-list-item-title>
+						<v-list-item-title
+							>Charts</v-list-item-title
+						>
 					</v-list-item-content>
 				</v-list-item>
 				<v-list-item link dense to="/orders">
 					<v-list-item-action>
-						<v-icon>mdi-clipboard-arrow-right</v-icon>
+						<v-icon
+							>mdi-clipboard-arrow-right</v-icon
+						>
 					</v-list-item-action>
 					<v-list-item-content>
-						<v-list-item-title>Orders</v-list-item-title>
+						<v-list-item-title
+							>Orders</v-list-item-title
+						>
 					</v-list-item-content>
 				</v-list-item>
 				<v-list-item link dense to="/map">
 					<v-list-item-action>
-						<v-icon>mdi-map-marker-multiple</v-icon>
+						<v-icon
+							>mdi-map-marker-multiple</v-icon
+						>
 					</v-list-item-action>
 					<v-list-item-content>
-						<v-list-item-title>Map</v-list-item-title>
+						<v-list-item-title
+							>Map</v-list-item-title
+						>
 					</v-list-item-content>
 				</v-list-item>
 				<v-list-item link dense to="/settings">
@@ -139,7 +170,9 @@
 						<v-icon>mdi-settings</v-icon>
 					</v-list-item-action>
 					<v-list-item-content>
-						<v-list-item-title>Settings</v-list-item-title>
+						<v-list-item-title
+							>Settings</v-list-item-title
+						>
 					</v-list-item-content>
 				</v-list-item>
 				<v-list-item link dense to="/help">
@@ -147,7 +180,9 @@
 						<v-icon>mdi-help-circle</v-icon>
 					</v-list-item-action>
 					<v-list-item-content>
-						<v-list-item-title>Help</v-list-item-title>
+						<v-list-item-title
+							>Help</v-list-item-title
+						>
 					</v-list-item-content>
 				</v-list-item>
 				<v-list-item link dense to="/about">
@@ -155,7 +190,9 @@
 						<v-icon>mdi-border-all</v-icon>
 					</v-list-item-action>
 					<v-list-item-content>
-						<v-list-item-title>About</v-list-item-title>
+						<v-list-item-title
+							>About</v-list-item-title
+						>
 					</v-list-item-content>
 				</v-list-item>
 				<v-list-item link dense disabled>
@@ -163,7 +200,10 @@
 						<v-icon>mdi-border-none</v-icon>
 					</v-list-item-action>
 					<v-list-item-content>
-						<v-list-item-title>Disabled stuff</v-list-item-title>
+						<v-list-item-title
+							>Disabled
+							stuff</v-list-item-title
+						>
 					</v-list-item-content>
 				</v-list-item>
 			</v-list>
@@ -175,7 +215,11 @@
 
 		<v-snackbar v-model="logOutSnackbar" timeout="3000">
 			{{ logOutSnackbarText }}
-			<v-btn color="pink" text @click="logOutSnackbar = false">
+			<v-btn
+				color="pink"
+				text
+				@click="logOutSnackbar = false"
+			>
 				Close
 			</v-btn>
 		</v-snackbar>
@@ -228,7 +272,9 @@
 					: "mdi-arrow-left";
 			},
 			changeArrowOnExpandUserMenu: function() {
-				return this.userMenuExpanded === true ? "mdi-menu-down" : "mdi-menu-up";
+				return this.userMenuExpanded === true
+					? "mdi-menu-down"
+					: "mdi-menu-up";
 			},
 			notificationBadgeNum: function() {
 				return this.notificationItems.length;
