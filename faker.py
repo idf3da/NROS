7667ba64e4f8383d0d887f6db53d5f23462eeb0c
Lev <wot.k@mail.ru>
2020-03-18 14:21:40 +0300

    bd for flask

---
+++ faker.py
@@ -0,0 +1,61 @@
+import random
+import datetime
+
+from models import ProductItem
+
+
+class FakedData:
+    def __init__(self, product_items, location, date):
+        self.product_items = product_items
+        self.location = location
+        self.date = date
+
+
+class Faker:
+    def do(self, locations_count, products_count, data_package_count, days_count):
+        locations = [i for i in range(locations_count)]
+        products = [i for i in range(products_count)]
+        probabilities = [
+            [(random.random() + 0.8 if random.random() > 0.9 else random.random()) for j in range(products_count)]
+            for i in range(locations_count)]
+        result = []
+        for index, location in enumerate(locations):
+            for day in range(days_count):
+                result.append(self.generate_data(products, probabilities[index], data_package_count, location, day))
+        return result
+
+    def generate_data(self, products, probabilities, count, location, date):
+        items = []
+        rand_sum = 0.0
+        for i in range(len(products)):
+            items.append([probabilities[i] + rand_sum, products[i]])
+            rand_sum += probabilities[i]
+        items.sort()
+        product_count = [0 for i in range(len(items))]
+        for i in range(count):
+            p = random.random() * rand_sum
+            l, r = -1, len(items)  # binary search
+            while r - l > 1:
+                m = (l + r) // 2
+                if items[m][0] < p:
+                    l = m
+                else:
+                    r = m
+            product_count[r] += 1
+        data = []
+        for index, count in enumerate(product_count):
+            # print(index, ':', count, end=' ')
+            if count:
+                data.append(ProductItem(items[index][1], count))
+        return FakedData(data, location, date)
+
+
+random.seed(datetime.datetime.now().microsecond)
+faker = Faker()
+result = faker.do(10, 20, 100, 3)
+for data in result:
+    print('location: ', data.location)
+    print('date: ', data.date)
+    for item in data.product_items:
+        print(item.id, ':', item.count, end=' | ')
+    print()
---
+++ models.py
@@ -0,0 +1,60 @@
+from my_flask_app import db
+
+tags = db.Table('tags',
+                db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
+                db.Column('page_id', db.Integer, db.ForeignKey('producttype.id'))
+                )
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class ProductType(db.Model):  # singletone object: Молоко, Домик в деревне, 100 бурлей, 1л, молочные продукты
+    id = db.Column(db.Integer, primary_key=True)
+    count = db.Column(db.Integer)
+    name = db.Column(db.String(100))
+    price = db.Column(db.Integer)
+    volume = db.Column(db.Integer)
+    tags = db.relationship('Tag', secondary=tags, backref=db.backref('product_types', lazy='dynamic'), lazy='dynamic')
+    '''возможно стоит изменить lazy='dynamic', чтобы получать не запрос '''
+    ''' если мы хотим возможность брать все ProductItems от ProductType, то надо добавить
+        items = db.relationship('productitem', backref='product_type', lazy='dynamic')'''
+
+
+class ProductItem(db.Model):  # Молоко: 2 шт.
+    id = db.Column(db.Integer, primary_key=True)
+    count = db.Column(db.Integer)
+    product_type_id = db.Column(db.Integer, db.ForeignKey('producttype.id'))
+
+
+class ProductGroup(db.Model):  # [Молоко: 2шт, Кефир: 3шт]
+    id = db.Column(db.Integer, primary_key=True)
+    products = db.relationship('productitem', backref='product_group', lazy='dynamic')
+
+
+class Location:
+    id = db.Column(db.Integer)
+    address = db.Column(db.String(100))
+    '''
+    надо подумать как мы будем реализовывать локацию
+    '''
+
+
+class Property(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String('100'))
+    location_id = db.Column(db.Integer, db.ForeignKey('location.id'))
+    storage_capacity = db.Column(db.Integer)
+
+
+class Warehouse(Property, db.Model):
+    """
+    не знаю будет ли он чем то отличаться в модели
+    """
+
+
+class RetailPoint(Property, db.Model):
+    """
+        не знаю будет ли он чем то отличаться в модели
+        """
