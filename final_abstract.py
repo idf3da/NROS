33e0a33b29393a29fd69d16d5d89e4d11c0b4f7a
Lev <wot.k@mail.ru>
2020-03-18 14:43:52 +0300

    Revert "Merge branch 'bd_abstract'"
    This reverts commit eff8b1b0dbc12e7b3f8ef3d991f1393f02198454, reversing
    changes made to 31e4294783667fce81226e5e96896c25f137ae8c.

--- final_abstract.py
+++ final_abstract.py
@@ -5,8 +5,7 @@ class Property():
         self.Location = "None"
         self.StorageCapacity = "0 kg"
 
-
-class ProductType:  # singletone object
+class Product():
     def __init__(self, ID, Count, Name, Tags, Price, Volume):
         self.ID = ID
         self.Count = Count
@@ -15,46 +14,37 @@ class ProductType:  # singletone object
         self.Price = Price
         self.Volume = Volume
 
+class ProductGroup():
+    def __init__(self, ID, Products_IDs):
+        self.product_type = product_IDS #[id_хлеб ид_молоко]
+        self.counts = [2,1] 
+        
 
-class ProductItem:  # Молоко: 2 шт.
-    def __init__(self, ID, count):
-        self.ID = ID
-        self.count = count
-        # ...
-
-
-class ProductGroup:  # [Молоко: 2шт, Кефир: 3шт]
-    def __init__(self, products):  # products - list ProductItem
-        self.products = products
-
-
-class Warehouse(Property):  # ! WH
+class Warehouse(Property): # ! WH
     def __init__(self, ID, Location, StorageCapacity):
         self.ID = ID
         self.Name = "NoneName"
         self.Location = Location
         self.StorageCapacity = StorageCapacity
-
+    
     def recieve_product(self):
         pass
 
     def send_product(self):
         pass
 
-
-class RetailPoint(Property):  # ! RP
+class RetailPoint(Property): # ! RP
     def __init__(self, ID, Location, StorageCapacity):
         self.ID = ID
         self.Name = "NoneName"
         self.Location = Location
         self.StorageCapacity = StorageCapacity
-
+    
     def recieve_product(self):
         pass
 
     def send_product(self):
         pass
-
 
 class NROS():
     def __init__(self):
