dabcaaccdd6ecfd93c929c266b5a983a063dfc24
Lev <wot.k@mail.ru>
2020-03-23 22:21:47 +0300

    databse_changed

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     FLASK_APP = 'app.py'
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///sqlite_db.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:admin@localhost/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
