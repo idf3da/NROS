b51452c33f67cf9eec3b4029249acb801b444fc1
Тулявко Лев <wot.k@mail.ru>
2020-04-25 13:20:51 +0000

    Update __init__.py

--- api_backend/myapp/__init__.py
+++ api_backend/myapp/__init__.py
@@ -1,17 +1,19 @@
+from config import Config
 from flask import Flask
+from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
-from flask_cors import CORS
-from myapp.models import *
-from myapp import routes
-from myapp import api_routes
 
 app = Flask(__name__)
 app.config.from_object(Config)
 api = Api(app)
 db = SQLAlchemy(app)
 
+from myapp.models import *
+
 migrate = Migrate(app, db)
 CORS(app)
+
+from myapp import routes
+from myapp import api_routes
