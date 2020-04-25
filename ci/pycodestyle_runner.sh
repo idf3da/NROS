3564e7ae3eb784477671d3a64d801dd74489148c
Тулявко Лев <wot.k@mail.ru>
2020-04-25 11:42:41 +0000

    Update pycodestyle_runner.sh

--- ci/pycodestyle_runner.sh
+++ ci/pycodestyle_runner.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-
 mkdir public
-pycodestyle . --statistics > public/pycodestyle.txt
-exit 0
+pip install -r ./pycodestyle_requirements.txt
+pycodestyle ../ --count -qq &> ./public/pycodestyle.txt
+OUTPUT=$(cat ./public/pycodestyle.txt)
+echo "$OUTPUT"
+if [[ "$OUTPUT" == '' ]]
+then
+    anybadge --label=pep8 --value=passing --file=public/pycodestyle.svg passing=green failing=red
+else
+    anybadge --label=pep8 --value=failing --file=public/pycodestyle.svg passing=green failing=red
+fi
+exit 1
