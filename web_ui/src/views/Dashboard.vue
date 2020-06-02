2e9169c85787bfb4830bb57b3d6529c18509fae5
IDF3DA97DF <defolt_17@mail.ru>
2020-06-02 18:34:09 +0300

    New data table

--- web_ui/src/views/Dashboard.vue
+++ web_ui/src/views/Dashboard.vue
@@ -1,6 +1,11 @@
 <template>
 	<div class="home">
 		<v-container fluid>
+			{{ product_items }}
+			<br />
+			{{ property_items }}
+			<br />
+			{{ store_data[0].productArr }}
 			<v-row align="start" justify="space-around">
 				<v-col>
 					<v-card class="pa-1" outlined tile :loading="switch1">
@@ -9,7 +14,7 @@
 							<v-spacer></v-spacer>
 							<v-text-field v-model="search1" append-icon="mdi-magnify" label="Search" hide-details></v-text-field>
 						</v-card-title>
-						<v-data-table :height="185" :headers="product_headers" :items="product_data" :search="search1"></v-data-table>
+						<v-data-table :height="185" :headers="product_items_headers" :items="product_items" :search="search1"></v-data-table>
 					</v-card>
 				</v-col>
 				<v-col>
@@ -40,19 +45,20 @@
 							<v-switch class="mr-5" v-model="switch1" color="blue" label="Use seasonal algorithm"></v-switch>
 							<v-btn color="success" :loading="switch1" large>Make <br />New suggestion</v-btn>
 						</v-card-title>
-						<v-data-table :headers="store_headers" :items="store_data" :expanded.sync="expanded" item-key="shopID" show-expand :search="table_search" :options.sync="table_pagination">
+						<v-data-table :headers="store_headers" :items="store_data" item-key="shopID" show-expand :search="table_search" :options.sync="table_pagination">
 							<template v-slot:top>
 								<v-toolbar flat>
 									<v-toolbar-title>Your properties</v-toolbar-title>
 								</v-toolbar>
 							</template>
-							<template v-slot:expanded-item="{}">
+
+							<template v-slot:expanded-item="{ item }">
 								<v-spacer></v-spacer>
 								<td :colspan="2">
-									<v-data-table hide-default-footer :headers="product_headers" :items="product_data" :items-per-page="1000" elevation-0>
+									<v-data-table hide-default-footer :height="300" :headers="property_product_headers" :items="item.productArr" :items-per-page="100" elevation-0>
 										<template v-slot:top>
 											<v-toolbar flat>
-												<v-toolbar-title caption>Properties products</v-toolbar-title>
+												<v-toolbar-title caption>{{ each_store_products }}</v-toolbar-title>
 											</v-toolbar>
 										</template>
 									</v-data-table>
@@ -113,18 +119,29 @@
 				table_pagination: {},
 				table_selected: [],
 				store_headers: [
-					{ text: "Name", value: "name" },
+					{ text: "Store ID", value: "point_id" },
 					{ text: "Address", value: "address" },
 					{ text: "Longtitude", value: "longtitude" },
 					{ text: "Latitude", value: "latitude" },
 					{ text: "Fullness", value: "fullness" },
 					{ text: "Capacity", value: "capacity" },
+					{ text: "Minimum", value: "minimum" },
 				],
-				product_headers: [
+				product_items_headers: [
 					{ text: "Name", value: "name" },
+					{ text: "Product ID", value: "product_type_id" },
 					{ text: "price", value: "price" },
 					{ text: "Popular season", value: "seasonDay" },
+					{ text: "Store address", value: "address" },
 				],
+				property_product_headers: [
+					{ text: "Name", value: "name" },
+					{ text: "Product ID", value: "product_type_id" },
+					{ text: "price", value: "price" },
+					{ text: "Popular season", value: "seasonDay" },
+					{ text: "Has neural model", value: "lstm" },
+				],
+
 				store_data: [
 					{
 						name: "Магазин 1",
@@ -133,9 +150,38 @@
 						latitude: 0,
 						fullness: 30,
 						capacity: 14,
-						minRequired: 10,
-						shop: false,
-						shopID: 1,
+						minimum: 10,
+						point_id: 1,
+						productArr: [
+							{
+								name: "Bread",
+								product_type_id: "976254762954",
+								price: 10,
+								seasonDay: 365,
+								lstm: true,
+							},
+							{
+								name: "Bread",
+								product_type_id: "29654776592",
+								price: 20,
+								seasonDay: 365,
+								lstm: true,
+							},
+							{
+								name: "Bread",
+								product_type_id: "79652457962",
+								price: 50,
+								seasonDay: 365,
+								lstm: true,
+							},
+							{
+								name: "Bread",
+								product_type_id: "786542729654",
+								price: 80,
+								seasonDay: 365,
+								lstm: true,
+							},
+						],
 					},
 					{
 						name: "Магазин 2",
@@ -144,69 +190,72 @@
 						latitude: 0,
 						fullness: 80,
 						capacity: 27,
-						minRequired: 15,
-						shop: true,
-						shopID: 2,
+						minimum: 15,
+						point_id: 2,
+						productArr: [
+							{
+								name: "Bread",
+								product_type_id: "3769539765",
+								price: 60,
+								seasonDay: 365,
+								lstm: true,
+							},
+							{
+								name: "Butter",
+								product_type_id: "93796",
+								price: 11,
+								seasonDay: 365,
+								lstm: true,
+							},
+							{
+								name: "Milk",
+								product_type_id: "39767396",
+								price: 34,
+								seasonDay: 365,
+								lstm: true,
+							},
+							{
+								name: "???",
+								product_type_id: "265848625",
+								price: 66,
+								seasonDay: 365,
+								lstm: true,
+							},
+							{
+								name: "",
+								product_type_id: "268862",
+								price: 0,
+								seasonDay: 365,
+								lstm: true,
+							},
+						],
 					},
 				],
-				product_data: [
-					{
-						name: "Bread",
-						price: 60,
-						seasonDay: 365,
-					},
-					{
-						name: "Ice cream sandwich",
-						price: 140,
-						seasonDay: 365,
-					},
-					{
-						name: "Eclair",
-						price: 80,
-						seasonDay: 365,
-					},
-					{
-						name: "Cupcake",
-						price: 70,
-						seasonDay: 365,
-					},
-					{
-						name: "Gingerbread",
-						price: 40,
-						seasonDay: 365,
-					},
-					{
-						name: "Jelly bean",
-						price: 110,
-						seasonDay: 365,
-					},
-					{
-						name: "Lollipop",
-						price: 30,
-						seasonDay: 365,
-					},
-					{
-						name: "Honeycomb",
-						price: 40,
-						seasonDay: 365,
-					},
-					{
-						name: "Donut",
-						price: 45,
-						seasonDay: 365,
-					},
-					{
-						name: "KitKat",
-						price: 65,
-						seasonDay: 365,
-					},
-				],
+				product_items: [],
+				property_items: [],
 			};
 		},
 		created() {
 			axios.get("http://127.0.0.1:5000/api/locations").then((response) => {
 				this.locations = response.data.locations;
 			});
+			axios
+				.get("http://127.0.0.1:5000/api/product_types", {
+					Authorization: localStorage.getItem("token") || "",
+				})
+				.then((response) => {
+					this.product_items = response.data.product_types;
+				});
+
+			axios
+				.post("http://127.0.0.1:5000/api/user/integrate", {
+					Authorization: localStorage.getItem("token") || "",
+				})
+				.then((response) => {
+					this.property_items = response.data;
+				});
+
+			console.log(this.product_items);
 		},
 	};
 </script>
